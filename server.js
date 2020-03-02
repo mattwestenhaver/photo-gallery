b7b17b0ad79a32d0861ff54f3b38697318a73c86
mattwestenhaver <matthew.westenhaver@gmail.com>
2020-03-01 23:02:06 -0500

    heroku debugging

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser.json())
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "./client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 mongoose.connect(MONGODB_URI, {
