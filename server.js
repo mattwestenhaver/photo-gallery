5b8c21b7a8be06061024e5a781e8bfcb9965f497
mattwestenhaver <matthew.westenhaver@gmail.com>
2020-03-01 21:53:01 -0500

    added heroku start scripts

--- server.js
+++ server.js
@@ -16,9 +16,14 @@ const
   PORT = process.env.PORT || 3001 
 ;
 
+
 app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
+});
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
