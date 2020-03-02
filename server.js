56221778829a34cd63e726cd44adf39d94fa6dac
mattwestenhaver <matthew.westenhaver@gmail.com>
2020-03-01 22:18:29 -0500

    fixed client path again

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const
 app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "./client/build/index.html"));
 });
